427c8354e73d9f018b15cc7a9f3f3139cf8c03fc
Deniskiplimo <deniskiplimo816@example.com>
2025-04-08 22:09:39 +0300

    Fixed TransformStream issue for Node.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { graphqlHTTP } = require("express-graphql");
 const MODELS = require("./models/llama");
 const os = require('os'); 
 require('web-streams-polyfill');
-  
+   
 const { Transform } = require('stream'); 
 const { body, validationResult ,query} = require("express-validator");
 const PORT = process.env.PORT || 3000;
